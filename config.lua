f0f919c617cbf121b680bcf10d28e0a0c25d5fff
BulgaRpl <69915160+BulgaRpl@users.noreply.github.com>
2023-07-18 23:28:50 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,167 @@
+-- This is just an example of the config for the bulgar_mining script
+-- In the example config some settings have been removed that you will get only after getting the script 
+-- This is just an example of the config for the bulgar_mining script
+-- In the example config some settings have been removed that you will get only after getting the script 
+-- This is just an example of the config for the bulgar_mining script
+-- In the example config some settings have been removed that you will get only after getting the script 
+-- This is just an example of the config for the bulgar_mining script
+-- In the example config some settings have been removed that you will get only after getting the script 
+
+
+Config = {}
+
+-- Select Framework
+Config.Framework = 'VORP' -- or 'REDEMRPREBOOT' or 'REDEMRP' or 'QBRCore' or 'RSGCore' or 'Other'
+
+-- Mining Blips + Tools NPCs Locations ( Every Mine has own NPC that gives u Tools to work with. )
+Config.BlipName = 'CONFIGURED AFTER U GET THE SCRIPT'
+Config.BlipSprite = 'CONFIGURED AFTER U GET THE SCRIPT'
+Config.BlipScale = 'CONFIGURED AFTER U GET THE SCRIPT'
+Config.Blips = {
+    { x = 00.00, y = 00.00, z = 00.00, npcmodel = 'CONFIGURED AFTER U GET THE SCRIPT', npcheading = 00.00 }, -- 'CONFIGURED AFTER U GET THE SCRIPT'
+    { x = 00.00, y = 00.00, z = 00.00, npcmodel = 'CONFIGURED AFTER U GET THE SCRIPT', npcheading = 00.00 }, -- 'CONFIGURED AFTER U GET THE SCRIPT'
+	-- Other Fell Free to Add...
+}
+Config.EnableGetToolNPC = true -- For better immersion, prompt can be set bellow in Config.EnableGetToolPrompt
+
+-- Mining Markers Settings from Config.MiningLocations
+Config.MarkerDisplayDistance = 'CONFIGURED AFTER U GET THE SCRIPT'
+Config.MarkerDisplayType = 'CONFIGURED AFTER U GET THE SCRIPT'
+Config.MarkerDisplaySize = 'CONFIGURED AFTER U GET THE SCRIPT'
+Config.MarkerDisplayHeight = 'CONFIGURED AFTER U GET THE SCRIPT'
+Config.MarkerDisplayColor = 'CONFIGURED AFTER U GET THE SCRIPT'
+Config.MarkerRotate = false
+
+-- Mining Positions
+Config.MiningPositionBlip = true -- After u pick tool mining positions will be on radar/map.
+Config.MiningPositionSprite = 'CONFIGURED AFTER U GET THE SCRIPT'
+Config.MiningPositionSpriteID = 'CONFIGURED AFTER U GET THE SCRIPT'
+Config.MiningPositionPointSprite = 'CONFIGURED AFTER U GET THE SCRIPT'
+Config.MiningLocations = {
+    [1] =   { x = 00.00, y = 00.00, z = 00.00, h = 00.00, name = 'CONFIGURED AFTER U GET THE SCRIPT' },
+    [2] =   { x = 00.00, y = 00.00, z = 00.00, h = 00.00, name = 'CONFIGURED AFTER U GET THE SCRIPT' },
+    [3] =   { x = 00.00, y = 00.00, z = 00.00, h = 00.00, name = 'CONFIGURED AFTER U GET THE SCRIPT' },
+    [4] =   { x = 00.00, y = 00.00, z = 00.00, h = 00.00, name = 'CONFIGURED AFTER U GET THE SCRIPT' },
+    [5] =   { x = 00.00, y = 00.00, z = 00.00, h = 00.00, name = 'CONFIGURED AFTER U GET THE SCRIPT' },
+    [6] =   { x = 00.00, y = 00.00, z = 00.00, h = 00.00, name = 'CONFIGURED AFTER U GET THE SCRIPT' },
+    [7] =   { x = 00.00, y = 00.00, z = 00.00, h = 00.00, name = 'CONFIGURED AFTER U GET THE SCRIPT' },
+    [8] =   { x = 00.00, y = 00.00, z = 00.00, h = 00.00, name = 'CONFIGURED AFTER U GET THE SCRIPT' },
+    [9] =   { x = 00.00, y = 00.00, z = 00.00, h = 00.00, name = 'CONFIGURED AFTER U GET THE SCRIPT' },
+    [10] =  { x = 00.00, y = 00.00, z = 00.00, h = 00.00, name = 'CONFIGURED AFTER U GET THE SCRIPT' },
+    [11] =  { x = 00.00, y = 00.00, z = 00.00, h = 00.00, name = 'CONFIGURED AFTER U GET THE SCRIPT' },
+    [12] =  { x = 00.00, y = 00.00, z = 00.00, h = 00.00, name = 'CONFIGURED AFTER U GET THE SCRIPT' },
+    [13] =  { x = 00.00, y = 00.00, z = 00.00, h = 00.00, name = 'CONFIGURED AFTER U GET THE SCRIPT' },
+    [14] =  { x = 00.00, y = 00.00, z = 00.00, h = 00.00, name = 'CONFIGURED AFTER U GET THE SCRIPT' },
+    [15] =  { x = 00.00, y = 00.00, z = 00.00, h = 00.00, name = 'CONFIGURED AFTER U GET THE SCRIPT' },
+    [16] =  { x = 00.00, y = 00.00, z = 00.00, h = 00.00, name = 'CONFIGURED AFTER U GET THE SCRIPT' },
+    [17] =  { x = 00.00, y = 00.00, z = 00.00, h = 00.00, name = 'CONFIGURED AFTER U GET THE SCRIPT' },
+    [18] =  { x = 00.00, y = 00.00, z = 00.00, h = 00.00, name = 'CONFIGURED AFTER U GET THE SCRIPT' },
+    [19] =  { x = 00.00, y = 00.00, z = 00.00, h = 00.00, name = 'CONFIGURED AFTER U GET THE SCRIPT' },
+    [20] =  { x = 00.00, y = 00.00, z = 00.00, h = 00.00, name = 'CONFIGURED AFTER U GET THE SCRIPT' },
+    [21] =  { x = 00.00, y = 00.00, z = 00.00, h = 00.00, name = 'CONFIGURED AFTER U GET THE SCRIPT' },
+    [22] =  { x = 00.00, y = 00.00, z = 00.00, h = 00.00, name = 'CONFIGURED AFTER U GET THE SCRIPT' },
+    [23] =  { x = 00.00, y = 00.00, z = 00.00, h = 00.00, name = 'CONFIGURED AFTER U GET THE SCRIPT' },
+    [24] =  { x = 00.00, y = 00.00, z = 00.00, h = 00.00, name = 'CONFIGURED AFTER U GET THE SCRIPT' },
+    [25] =  { x = 00.00, y = 00.00, z = 00.00, h = 00.00, name = 'CONFIGURED AFTER U GET THE SCRIPT' },
+    [26] =  { x = 00.00, y = 00.00, z = 00.00, h = 00.00, name = 'CONFIGURED AFTER U GET THE SCRIPT' },
+    [27] =  { x = 00.00, y = 00.00, z = 00.00, h = 00.00, name = 'CONFIGURED AFTER U GET THE SCRIPT' },
+    [28] =  { x = 00.00, y = 00.00, z = 00.00, h = 00.00, name = 'CONFIGURED AFTER U GET THE SCRIPT' },
+    [29] =  { x = 00.00, y = 00.00, z = 00.00, h = 00.00, name = 'CONFIGURED AFTER U GET THE SCRIPT' },
+    [30] =  { x = 00.00, y = 00.00, z = 00.00, h = 00.00, name = 'CONFIGURED AFTER U GET THE SCRIPT' },
+    [31] =  { x = 00.00, y = 00.00, z = 00.00, h = 00.00, name = 'CONFIGURED AFTER U GET THE SCRIPT' },
+    [32] =  { x = 00.00, y = 00.00, z = 00.00, h = 00.00, name = 'CONFIGURED AFTER U GET THE SCRIPT' },
+    [33] =  { x = 00.00, y = 00.00, z = 00.00, h = 00.00, name = 'CONFIGURED AFTER U GET THE SCRIPT' },
+    [34] =  { x = 00.00, y = 00.00, z = 00.00, h = 00.00, name = 'CONFIGURED AFTER U GET THE SCRIPT' },
+    [35] =  { x = 00.00, y = 00.00, z = 00.00, h = 00.00, name = 'CONFIGURED AFTER U GET THE SCRIPT' },
+    [36] =  { x = 00.00, y = 00.00, z = 00.00, h = 00.00, name = 'CONFIGURED AFTER U GET THE SCRIPT' },
+    [37] =  { x = 00.00, y = 00.00, z = 00.00, h = 00.00, name = 'CONFIGURED AFTER U GET THE SCRIPT' },
+    [38] =  { x = 00.00, y = 00.00, z = 00.00, h = 00.00, name = 'CONFIGURED AFTER U GET THE SCRIPT' },
+    [39] =  { x = 00.00, y = 00.00, z = 00.00, h = 00.00, name = 'CONFIGURED AFTER U GET THE SCRIPT' },
+    [40] =  { x = 00.00, y = 00.00, z = 00.00, h = 00.00, name = 'CONFIGURED AFTER U GET THE SCRIPT' },
+	-- Other Fell Free to Add...
+}
+
+-- Prompts Settings
+Config.PromptTitle = 'CONFIGURED AFTER U GET THE SCRIPT'
+Config.PromptStartTitle = 'CONFIGURED AFTER U GET THE SCRIPT'
+Config.PromptStopTitle = 'CONFIGURED AFTER U GET THE SCRIPT'
+Config.PromptStartKey = 'CONFIGURED AFTER U GET THE SCRIPT'
+Config.PromptStopKey = 'CONFIGURED AFTER U GET THE SCRIPT'
+Config.EnableGetToolPrompt = true --  If false u will not be able to get the tools to work, but u can use this client trigger if u want to code it different for example as item --> 'CONFIGURED AFTER U GET THE SCRIPT'
+Config.GetToolPromptTitle = 'CONFIGURED AFTER U GET THE SCRIPT'
+Config.GetToolPromptStartTitle = 'CONFIGURED AFTER U GET THE SCRIPT'
+Config.GetToolPromptKey = 'CONFIGURED AFTER U GET THE SCRIPT'
+
+-- Mining Rewards + Drop Chance.
+Config.MinDrop = 'CONFIGURED AFTER U GET THE SCRIPT' -- Minimum amount of x item u can drop at once
+Config.MaxDrop = 'CONFIGURED AFTER U GET THE SCRIPT' -- Maximum amount of x item u can drop at once
+-- 50% Drop Chance Rewards
+Config.RewardsFree = {
+	{ item = "rock", label = "Rock" },
+	-- { item = "exampleitem", label = "Example Name" },
+}
+-- 35% Drop Chance Rewards
+Config.RewardsCommon = {
+	{ item = "iron_ore", label = "Iron Ore" },
+	-- { item = "exampleitem", label = "Example Name" },
+}
+-- 10% Drop Chance Rewards
+Config.RewardsRare = {
+    { item = "silver_ore", label = "Silver Ore" },
+	-- { item = "exampleitem", label = "Example Name" },
+}
+-- 3% Drop Chance Rewards
+Config.RewardsEpic = {
+    { item = "golden_nugget", label = "Golden Nugget" },
+	-- { item = "exampleitem", label = "Example Name" },
+}
+-- 2% Drop Chance Rewards
+Config.RewardsLegendary = {
+    { item = "diamond", label = "Diamond" },
+	-- { item = "exampleitem", label = "Example Name" },
+}
+
+-- Working Duration Settings
+Config.MinWorkDuration = 'CONFIGURED AFTER U GET THE SCRIPT'
+Config.MaxWorkDuration = 'CONFIGURED AFTER U GET THE SCRIPT'
+Config.EnableResting = true -- When Enabled u need to rest sometimes after work to start again.
+Config.ChanceToGetTired = 'CONFIGURED AFTER U GET THE SCRIPT' -- % Chance to get tired = need to rest before work.
+Config.MinRestDuration = 'CONFIGURED AFTER U GET THE SCRIPT'
+Config.MaxRestDuration = 'CONFIGURED AFTER U GET THE SCRIPT'
+Config.UseProgressBars = true -- This will use the code from bellow so if u want u can code own progressbars script.
+Config.ProgressBar = function()
+		-- 'CONFIGURED AFTER U GET THE SCRIPT'
+		-- 'CONFIGURED AFTER U GET THE SCRIPT'
+	if Config.Framework == 'VORP' then
+		-- 'CONFIGURED AFTER U GET THE SCRIPT'
+	elseif Config.Framework == 'REDEMRPREBOOT' then
+		-- 'CONFIGURED AFTER U GET THE SCRIPT'
+	elseif Config.Framework == 'REDEMRP' then
+		-- 'CONFIGURED AFTER U GET THE SCRIPT'
+	elseif Config.Framework == 'QBRCore' then
+		-- 'CONFIGURED AFTER U GET THE SCRIPT'
+	elseif Config.Framework == 'RSGCore' then
+		-- 'CONFIGURED AFTER U GET THE SCRIPT'
+	end
+end
+
+-- Translation
+Config.Language = {
+    [1] = {text = "CONFIGURED AFTER U GET THE SCRIPT",},
+    [2] = {text = "CONFIGURED AFTER U GET THE SCRIPT",},
+    [3] = {text = "CONFIGURED AFTER U GET THE SCRIPT",},
+    [4] = {text = "CONFIGURED AFTER U GET THE SCRIPT",},
+    [5] = {text = "CONFIGURED AFTER U GET THE SCRIPT",},
+    [6] = {text = "CONFIGURED AFTER U GET THE SCRIPT",},
+    [7] = {text = "CONFIGURED AFTER U GET THE SCRIPT",},
+}
+
+
+-- This is just an example of the config for the bulgar_mining script
+-- In the example config some settings have been removed that you will get only after getting the script 
+-- This is just an example of the config for the bulgar_mining script
+-- In the example config some settings have been removed that you will get only after getting the script 
+-- This is just an example of the config for the bulgar_mining script
+-- In the example config some settings have been removed that you will get only after getting the script 
+-- This is just an example of the config for the bulgar_mining script
+-- In the example config some settings have been removed that you will get only after getting the script 
